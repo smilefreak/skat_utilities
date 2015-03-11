38438685148a83c8c752cbede805d04b04aa104b
James Boocock <james.boocock@otago.ac.nz>
2015-03-11 21:44:01 +1300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name = "skat_utilities",
+    version = "0.1",
+    packages =['skat_util'],
+    author="James Boocock",
+    author_email="james.boocock@otago.ac.nz",
+    description="SKAT utilities (for rare variant testing)",
+    url="https://github.com/smilefreak/skat_utilities"
+)
+
+
+    
